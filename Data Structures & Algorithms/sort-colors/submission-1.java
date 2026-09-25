cd7a573f50b19e46d186b5c577308f6c24b7d079
Shri Harini J <shriharini1809@gmail.com>
2026-09-25 11:51:21 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            int j=i-1;
+            int key = nums[i];
+            while(j>=0 && nums[j]>key){
+                nums[j+1]= nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }
+        System.out.print(java.util.Arrays.toString(nums));
+    }
+}
